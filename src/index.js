6094a317d9debf6d2fa664de5625f1ae718c0f2f
Muthu Sanjai A S <muthusanjai874@gmail.com>
2025-11-03 12:42:44 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('🚀 CI/CD Pipeline working successfully!');
+  res.send('🚀 CI/CD Pipeline working successfully done!');
 });
 
 app.listen(3000, () => {
