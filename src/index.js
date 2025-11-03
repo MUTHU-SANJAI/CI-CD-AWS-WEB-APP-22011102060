7842751bca60a3d5cf11e64400b564e73b1e5133
Muthu Sanjai A S <muthusanjai874@gmail.com>
2025-11-03 13:49:10 +0530

    Updated Jenkinsfile for stable deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('🚀 CI/CD Pipeline working successfully done! 22');
+  res.send('🚀 CI/CD Pipeline working successfully done...!');
 });
 
 app.listen(3000, () => {
